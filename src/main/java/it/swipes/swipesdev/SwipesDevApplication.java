127728e65e0f820509ceba9e78c2a494f47327a2
macgood <bo2002277@gmail.com>
2022-06-05 16:57:26 +0300

    init

---
+++ src/main/java/it/swipes/swipesdev/SwipesDevApplication.java
@@ -0,0 +1,21 @@
+package it.swipes.swipesdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SwipesDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwipesDevApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello world";
+    }
+
+}
---
+++ src/test/java/it/swipes/swipesdev/SwipesDevApplicationTests.java
@@ -0,0 +1,13 @@
+package it.swipes.swipesdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipesDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
